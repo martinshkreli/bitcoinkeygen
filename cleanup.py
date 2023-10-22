64303751e2abac50154510c70e2d4ad988c70c84
Martin Shkreli <shkrelimartin@gmail.com>
2023-10-22 16:15:53 -0400

    added requirements.txt for the normies

--- cleanup.py
+++ cleanup.py
@@ -13,18 +13,22 @@ with open(input_file, mode='r', newline='') as infile, open(output_file, mode='w
     for row in reader:
         try:
             current_value = int(row[0])
+            # Check if current_value is the expected_next or if we encounter the same value again
+            while current_value > expected_next:
+                # If the value is greater than expected, we're missing some values in between. Skip the write.
+                expected_next += 1
+            # Only write the row if the current_value matches the expected_next
             if current_value == expected_next:
                 writer.writerow(row)
                 expected_next += 1
-            # Else, it's an out-of-sequence value, skip writing it to the output
         except ValueError:
             # Handle potential non-integer values gracefully
             print(f"Encountered a non-integer value {row[0]}. Skipping...")
+            # Wait for user input to continue
 
 input("Press Enter to continue...")
-filename = "1413000000.csv"
 
-with open(filename, 'r') as f:
+with open(output_file, 'r') as f:
     line_count = sum(1 for line in f)
 
-print(f"The file {filename} has {line_count} lines.")
+print(f"The file {output_file} has {line_count} lines.")
